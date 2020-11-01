4e80c7bbb93c110ee896ae3aca4f8f8b981d4692
Nathan Reed <58314490+justnat3@users.noreply.github.com>
2020-11-01 17:10:00 -0600

    Update unaluz_counter.kt

--- Kotlin/unaluz_counter.kt
+++ Kotlin/unaluz_counter.kt
@@ -1,50 +1,48 @@
-use std::io::prelude::*;                                                           
-
-fn main() -> Result<(), ()> {
-    answer("Do you wanna start counting")?;
-    let amount = read_num("How much do you want to count to? ");
-    println!("Press to enter to count!");
-    loop {
-        count(amount)?;
+fun main(args: Array<String>) {
+    println("Would you like to start counting?[y/n]")
+    var answer: Char = try{readLine()!!.toLowerCase()!![0]} catch(e: Exception) {'e'}
+    println(">>$answer\n")
+    
+    while(answer != 'y' && answer != 'n'){
+        println("...")
+        answer = try{readLine()!!.toLowerCase()!![0]} catch(e: Exception) {'e'}
+        println(">>$answer\n")
     }
-}
-
-fn count(num: usize) -> Result<(), ()> {
-    let mut tmp = String::new();
-    for i in 1..num+1 {
-        print!("{}", i);
-        std::io::stdout().flush().ok().expect("Could not flush stdout");
-        if i != num {
-            std::io::stdin().read_line(&mut tmp).expect("Invalid UTF-8 characters found in input");
+    
+    if (answer == 'n'){
+        println("Too bad, see ya")
+        System.exit(1)
+    }
+    
+    println("Up to what would you like to count?")
+    var top = try {readLine()!!.toInt()} catch(e: Exception) {-1}
+    println(">>$top\n")
+    
+    while(top < 0){
+        println("I need a natural number greater than zero pls")
+        top = try {readLine()!!.toInt()} catch(e: Exception) {-1}
+        println(">>$top\n")
+    }
+    
+    if(top == 0){
+        println("You could've just said No")
+        System.exit(1)
+    }
+    
+    println("Ok, let's count up to $top, press ENTER to advance")
+    var num = 0
+    var input: String? = "nothing"
+    
+    while(num < top){
+        input = readLine()
+        println(">>$input\n")
+        if(input.isNullOrEmpty()){
+            num = num.inc()
+            println("We're at $num out of $top")
+        }
+        else{
+            println("Try again")
         }
     }
-    println!("");
-    answer("Do you want to keep going")
-}
-
-fn read_num(prompt: &str) -> usize {
-    print!("{}", prompt);
-    loop {
-        std::io::stdout().flush().ok().expect("Could not flush stdout");
-        let mut input_str = String::new();
-        std::io::stdin().read_line(&mut input_str).expect("Invalid UTF-8 characters found in input");
-        match input_str.trim().parse::<usize>() {
-            Ok(num) => return num,
-            Err(_) => println!("Not a number, please try again."),
-        }
-    }
-}
-
-fn answer(prompt: &str) -> Result<(), ()> {
-    print!("{} [Y/n]: ", prompt);
-    std::io::stdout().flush().ok().expect("Could not flush stdout");
-    let mut input_str = String::new();
-    std::io::stdin().read_line(&mut input_str).expect("Invalid UTF-8 characters found in input");
-    match input_str.trim() {
-        "y" | "Y" => Ok(()),
-        _ => {
-            println!("Cancelled by user");
-            Err(())
-        },
-    }
+    println("Done!")
 }
